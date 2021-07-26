6a59e7e43fe661af6b0330dc6e5d3d40d93463db
Daiane Silva <daianemsilva09@gmail.com>
2021-07-26 17:30:59 -0300

    Usando o TypeScript e tipos básicos

---
+++ tipos.js
@@ -0,0 +1,4 @@
+"use strict";
+//boolean
+var contaPaga = false;
+// number 
---
+++ tipos.ts
@@ -0,0 +1,53 @@
+//boolean
+
+const contaPaga: boolean = false;
+
+// number 
+const idade: number = 23;
+const avaliacao: number = 4.5;
+
+//string
+const nome: string = 'Dai Silva';
+
+//array
+const idades: number[] = [23, 28, 12, 13, 14];
+const idade2: Array<number> = [199, 19, 190, 67];
+
+//tuple
+let jogadores: [string, number, boolean];
+jogadores = ['Dai', 3, true];
+
+//enum
+enum StatusAprovacao {
+    Aprovado = '001',
+    Pendente = '002',
+    rejeitado = '003'
+}
+const statusDaAprovacao: StatusAprovacao = StatusAprovacao.Aprovado;
+
+//Any
+const retornoDaAPI: any[] = [123, 'Dai', false];
+const retornoDaAPI2 = any = {
+    // .....
+};
+
+//Void 
+function printarNaTela(msg: string): void {
+    console.log(msg);
+}
+
+//Null e Undefined
+
+const u: undefined = undefined;
+const n: null = null;
+
+//Object
+function criar(objecto: object) {
+    //.
+}
+criar({ propriedade: 1, })
+ // criar ('Victor')// da erro
+
+
+
+
