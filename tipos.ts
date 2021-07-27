5b2a2e7b53ab962969725685b635edc2a6b5bf1c
Daiane Silva <daianemsilva09@gmail.com>
2021-07-26 22:11:32 -0300

    08 - Type Assertion

--- tipos.ts
+++ tipos.ts
@@ -97,3 +97,11 @@ const contato: Contato = {
     nome: 'Dai',
     telefone1: '12345'
 }
+
+//Type Assertion
+const minhaIdade: any = 23;
+(minhaIdade as number).toString();
+
+//const input = document.getElementById("numero1") as HTMLInputElement;
+const input = <HTMLInputElement>document.getElementById("numero1");
+console.log(input.value);
